8f1b626e299c00e679e01ba66b7ec90c0094138b
ParTi5qn <33070649+ParTi5qn@users.noreply.github.com>
2020-01-12 22:25:29 +0100

    Cleaning 1

--- main.js
+++ main.js
@@ -1,74 +1,59 @@
-require('dotenv').config({path:'.env'})
+require('dotenv').config();
 const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
-const port = 8000;
 
-const roc = "https://plaza2.rocvantwente.nl/s/plaza/SitePages/rooster.aspx?jaarweek=&Klas=8AA1&locatie=ALSU";
+let roc = process.env.ROC;
+const url = "https://plaza2.rocvantwente.nl/s/plaza/SitePages/rooster.aspx?jaarweek=&Klas=8AA1&locatie=ALSU"
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
-let data = {};
+const xTable = process.env.XTABLE;
 
-//console.log({username, password});
+console.log(process.env);
 
-let b = false;
-
-async function getData(url = "https://plaza2.rocvantwente.nl/s/plaza/SitePages/rooster.aspx?jaarweek=&Klas=8AA1&locatie=ALSU"){
-     const browser = await puppeteer.launch({
-	executablePath: 'chromium-browser',
+async function getData() {
+    
+    // Launch puppeteer browser 
+    const browser = await puppeteer.launch({
         headless: false,
-        args: ['--start-fullscreen']
-     });
+        defaultViewport: {
+            width: 1000,
+            height: 700
+        },
+    });
 
+    // Create new page
     const page = await browser.newPage();
+    // Goto the specified url
     await page.goto(url);
-    await page.authenticate(username, password);
-   
 
-    const hrefElement = await page.$eval('#gebruikersnaam', el => el);
+    // Fill in the login information and click the login button.
     await page.type("#gebruikersnaam", username);
     await page.type("#wachtwoord", password);
     await Promise.all([
         page.click('[id="inloggen"]'),
-        page.waitForNavigation({ waitUntil: 'networkidle0' })
-    ]).then(() => { b = true});
+        page.waitForNavigation({
+            waitUntil: 'networkidle0'
+        })
+    ]);
 
-        if(b == true){
-	const [ok] = await page.$x('//*[@id="ctl42_g_42ada3a4_d403_4da4_b79b_5c6fde1cb161_RoosterPageRooster"]/table');
-        const txt = await ok.getProperty('textContent');
-        data = await txt.jsonValue();
-        page.setContent(rt);
-	}
+    // Wait till the page is loaded
+    await sleep(2000);
+    // Wait for the table which we want to scrape to be loaded
+    await page.waitForXPath(xTable);
+    // Get that element and print it to the page
+    const [ok] = await page.$x(xTable);
+    const txt = await ok.getProperty('textContent');
+    const rawText = await txt.jsonValue();
+    await page.setContent(rawText);
+    console.log({rawText});
+    return rawText;
 }
 
-getData(roc);
+// Sets the script to sleep
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-
-
-app.get('/', (req, res) => {res.send(data) });
-app.listen(port, () => {});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(8000, () => getData());
